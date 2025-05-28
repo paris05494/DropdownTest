980cce0acdf160df6b38a51f0f6780910365c52c
paris05494 <parisza1990@gmail.com>
2025-05-28 22:18:59 +0700

    Add project files.

---
+++ Dropdown/Models/BaseUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dropdown.Models
+{
+    public class BaseUnit
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
---
+++ Dropdown/ViewModels/MainVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Dropdown.Models;
+
+namespace Dropdown.ViewModels
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private string _searchText;
+        private BaseUnit _selectedItem;
+        private int _selectedIndex;
+
+        public ObservableCollection<BaseUnit> AllItems { get; set; }
+        public ObservableCollection<BaseUnit> FilteredItems { get; set; }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                if (_searchText != value)
+                {
+                    _searchText = value;
+                    OnPropertyChanged();
+                    FilterItems();
+                }
+            }
+        }
+
+        public BaseUnit SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                if (_selectedItem != value)
+                {
+                    _selectedItem = value;
+                    OnPropertyChanged();
+                    SelectedIndex = FilteredItems.IndexOf(_selectedItem);
+                }
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                if (_selectedIndex != value)
+                {
+                    _selectedIndex = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public MainVM()
+        {
+            AllItems = new ObservableCollection<BaseUnit>
+            {
+                new BaseUnit { Id = 1, Name = "Apple" },
+                new BaseUnit { Id = 2, Name = "Banana" },
+                new BaseUnit { Id = 3, Name = "Cherry" },
+                new BaseUnit { Id = 4, Name = "Date" }
+            };
+            FilteredItems = new ObservableCollection<BaseUnit>(AllItems);
+        }
+
+        private void FilterItems()
+        {
+            FilteredItems.Clear();
+            foreach (var item in AllItems.Where(i => string.IsNullOrWhiteSpace(SearchText) || i.Name.ToLower().Contains(SearchText.ToLower())))
+                FilteredItems.Add(item);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
